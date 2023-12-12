7156d7947d7f1adb2314173a7d23a4a5f6806523
SzymonRogozinski <pokeszymon.pl@gmail.com>
2023-12-12 21:45:03 +0100

    Add moving pieces

--- src/Components/ChessBoard/Cell.tsx
+++ src/Components/ChessBoard/Cell.tsx
@@ -2,24 +2,23 @@ import { CellObject } from "./ChessBoard";
 
 //Translating fen to file name
 const pieceDict:{[key:string]:string}={
-    "p":"w-pawn",
-    "r":"w-rook",
-    "n":"w-knight",
-    "b":"w-bishop",
-    "q":"w-queen",
-    "k":"w-king",
-    "P":"b-pawn",
-    "R":"b-rook",
-    "N":"b-knight",
-    "B":"b-bishop",
-    "Q":"b-queen",
-    "K":"b-king",
-    "_":"space"
+    "P":"w-pawn",
+    "R":"w-rook",
+    "N":"w-knight",
+    "B":"w-bishop",
+    "Q":"w-queen",
+    "K":"w-king",
+    "p":"b-pawn",
+    "r":"b-rook",
+    "n":"b-knight",
+    "b":"b-bishop",
+    "q":"b-queen",
+    "k":"b-king"
 };
 
 function Cell({color,cell,callback}:{color:string,cell:CellObject,callback:(cell:CellObject) => void}){
-    return <div className={color} onClick={()=>callback(cell)}>
-        <img className={`PieceImg ${pieceDict[cell.piece]}`}></img>
+    return <div className={`${color} ${cell.available?"available":""}`} onClick={()=>callback(cell)}>
+        <img className={`PieceImg ${pieceDict[cell.piece]} ${cell.choosed===true?"choosed":""}`}></img>
         </div>
 }
 
--- src/Components/ChessBoard/ChessBoard.tsx
+++ src/Components/ChessBoard/ChessBoard.tsx
@@ -1,21 +1,33 @@
 import './ChessBoard.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Chess}  from 'chess.js';
 import { log } from 'console';
 import Cell from './Cell';
 
 const col=["a","b","c","d","e","f","g","h"];
-const ver=[8,7,6,5,4,3,2,1];
+
+const reverseCol:{[key:string]:number}={
+    "a":0,
+    "b":1,
+    "c":2,
+    "d":3,
+    "e":4,
+    "f":5,
+    "g":6,
+    "h":7
+};
 
 //Class for storing data from one cell in chess board
 export class CellObject {
     pos: string;
     piece: string;
     choosed:boolean;
+    available:boolean;
     constructor(pos:string, piece:string) {
         this.pos = pos;
         this.piece = piece;
         this.choosed = false;
+        this.available = false;
     }
 }
 
@@ -25,7 +37,7 @@ function makeChess(fenString:string):CellObject[]{
     const pieces=Array.from(fenPieces);
     
     let board:CellObject[] =[];   //Chess board
-    let versCounter=0;      //Verse counter
+    let versCounter=8;      //Verse counter
     let counter:number=0;   //Column counter
     let i=0;                //Iterator
     while(i<pieces.length){
@@ -33,13 +45,13 @@ function makeChess(fenString:string):CellObject[]{
         let jump=parseInt(item);    //if nober, how many spaces
         //if item is not a number
         if(Number.isNaN(jump)){
-            let key=col[counter]+ver[versCounter];
+            let key=col[counter]+versCounter;
             let cell=new CellObject(key,item);
             board.push(cell);
             counter++;
         }else{
             for(let j=0;j<jump;j++){
-                let key=col[counter]+ver[versCounter];
+                let key=col[counter]+versCounter;
                 let cell=new CellObject(key,"_"); //Not " ", because css make cell not equal size
                 board.push(cell);
                 counter++;
@@ -47,7 +59,7 @@ function makeChess(fenString:string):CellObject[]{
         }
         //if verse ended
         if(counter===8){
-            versCounter++;
+            versCounter--;
             counter=0;
         }
         i++;
@@ -56,40 +68,74 @@ function makeChess(fenString:string):CellObject[]{
 };
 
 //Coloring cells
-function colorCell(id:number) {
+function colorCell(id:number):string {
     //if row is Even, move color scheme by one
-    if(Math.floor(id/8)%2==1){
+    if(Math.floor(id/8)%2===1){
         id++;
     }
-    return id%2==0?"black":"white";
+    return id%2===1?"black":"white";
 };
+
+//Calculate position of cell in table
+function reverseCellPosition(pos:string):number{
+    return reverseCol[pos.charAt(0)]+(parseInt(pos.charAt(1))-8)*-8;
+}
 
 function ChessBoard()
 {
     //Initialised component Chess
     let [chess,setChess]=useState(new Chess());
-    //Get FEN string
+    let [chessChanged,setChessChanged]=useState(false);
+    //Cell table
     let startChess=makeChess(chess.fen());
     //Set Board
     let [chessBoard,setChessBoard] = useState(startChess);
     //Clicked cell
     let [cellClicked,setCellClicked]=useState("");
 
-    const cellOnClick = (cell:CellObject) =>{
-        /*if(cell.pos===cellClicked){
+    useEffect(()=>{
+        console.log("reload");
+        let cellTable=makeChess(chess.fen());
+        setChessBoard(cellTable);
+    },[chessChanged])
 
-        }else{
+    const cellOnClick = (cell:CellObject) =>{
+        if(cell.piece==="_" && chessBoard[reverseCellPosition(cellClicked)].piece!=="_"){
+            try{
+                console.log({from: cellClicked, to: cell.pos});
+                console.log("before move");
+                console.log(chess.fen());
+                chess.move({from: cellClicked, to: cell.pos});
+                console.log("after move");
+                console.log(chess.fen());
+                setChessChanged(!chessChanged);
+            }catch(err){}
             
-        }*/
-        //unclicked previous one
-        chessBoard.forEach((c)=>{
-            if(c.pos===cellClicked){
-                c.choosed=false;
-            }
-        });
-        setCellClicked(cell.pos);
+        }
+        //ResetBoard
+        let chessCopy:CellObject[]=[];
+        chessBoard.forEach((c,i)=>{
+            chessCopy.push(c);
+            chessCopy[i].choosed=false;
+            chessCopy[i].available=false;
+        })
+        //Get avaiable cells
+        let currentPos:any=cell.pos;
+        let availableMoves=chess.moves({ square: currentPos });
+        if(availableMoves.length!=0){
+            availableMoves.forEach((m)=>{
+                //Set cell on legal position and change it to avaiable. slice(-2) to cut of unnecessary info
+                chessCopy[reverseCellPosition(m.slice(-2))].available=true;
+            })
+        }
+        //Set cell to choosed
         cell.choosed=true;
-        console.log(cell.pos);
+        setCellClicked(cell.pos);
+        console.log(cell.pos)
+
+        setChessBoard(chessCopy);
+
+        console.log(chessBoard);
     };
 
     return <div className="board">
