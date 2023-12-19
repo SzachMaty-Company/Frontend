c44a735020dfc7aed4f30a3408dfbaf74583951a
SzymonRogozinski <pokeszymon.pl@gmail.com>
2023-12-19 17:12:42 +0100

    Added pawn promotion

--- src/Components/ChessBoard/ChessBoard.tsx
+++ src/Components/ChessBoard/ChessBoard.tsx
@@ -100,10 +100,12 @@ function reverseCellPosition(piece:string,pos:string):number{
     return reverseCol[pos.charAt(0)]+(parseInt(pos.charAt(1))-8)*-8;
 }
 
+const stFen="rn1qkbnr/3pNppp/8/8/8/8/PpPPPPPP/R1BQKBNR w KQkq - 0 6";
+
 function ChessBoard()
 {
     //Initialised component Chess
-    let [chess,setChess]=useState(new Chess());
+    let [chess,setChess]=useState(new Chess(stFen));
     let [chessChanged,setChessChanged]=useState(false);
     //Cell table
     let startChess=makeChess(chess.fen());
@@ -111,15 +113,33 @@ function ChessBoard()
     let [chessBoard,setChessBoard] = useState(startChess);
     //Clicked cell
     let [cellClicked,setCellClicked]=useState("");
+    //Promotion
+    let [canPromote,setCanPromote]=useState("");
+    let [promoting,setPromoting]=useState(false);
 
     useEffect(()=>{
+        console.log(chess.fen());
         let cellTable=makeChess(chess.fen());
         setChessBoard(cellTable);
     },[chessChanged])
 
+    const promotionClick = (cell:CellObject)=>{
+        if(!promoting){
+            return;
+        }
+        let m=canPromote+cell.piece;
+        chess.move(m);
+        setChessChanged(!chessChanged);
+        setPromoting(false);
+    }
+
     const cellOnClick = (cell:CellObject) =>{
         console.log(cellClicked+" "+cell.pos)
+        if(promoting){
+            return;
+        }
         let ddd=chess.getCastlingRights(chess.turn()==="b"?BLACK:WHITE);
+        //Castling short
         if(((cellClicked==="e8" && cell.pos==="h8") || (cellClicked==="e1" && cell.pos==="h1")) && ddd.k){
             try{
                 console.log("O-O");
@@ -127,7 +147,9 @@ function ChessBoard()
                 setChessChanged(!chessChanged);
             }catch{}
             
-        }else if(((cellClicked==="e8" && cell.pos==="a8") || (cellClicked==="e1" && cell.pos==="a1")) && ddd.q){
+        }
+        //Castling long
+        else if(((cellClicked==="e8" && cell.pos==="a8") || (cellClicked==="e1" && cell.pos==="a1")) && ddd.q){
             try{
                 console.log("O-O-O");
                 chess.move("O-O-O");
@@ -135,6 +157,13 @@ function ChessBoard()
             }catch{}
         }
         if(cellClicked!=="" && chessBoard[reverseCellPosition("",cellClicked)].piece!=="_"){
+            //Promotion
+            if(canPromote!=="" && cellClicked===canPromote){
+                setPromoting(true);
+                setCanPromote(cellClicked+"x"+cell.pos+"=");
+            }else{
+                setCanPromote("");
+            }
             try{
                 console.log({from: cellClicked, to: cell.pos});
                 console.log("before move");
@@ -165,11 +194,19 @@ function ChessBoard()
                 if(move.indexOf("+")===m.length-1 || move.indexOf("#")===m.length-1){
                     move=move.substring(0,move.length-1);
                 }
+                //Add promotions
+                if(move.includes("=")){
+                    setCanPromote(cell.pos);
+                    move=move.slice(0,move.length-2);
+
+                    console.log(move);
+                }
                 //Set cell on legal position and change it to avaiable. slice(-2) to cut of unnecessary info
                 if(move!=="O-O-O" && move!=="O-O"){
                     move=move.slice(-2);
                 }
-                //Add promotions
+
+
                 chessCopy[reverseCellPosition(cell.piece,move)].available=true;
             })
         }
@@ -181,12 +218,25 @@ function ChessBoard()
         setChessBoard(chessCopy);
     };
 
-    return <div className="board">
-        {chessBoard.map((cell:CellObject,id:number) => (
-        <Cell key={cell.pos} color={colorCell(id)} cell={cell} callback={cellOnClick}></Cell>
-    ))}
-</div>;
-
+    return <>
+        <div className="board">
+            {chessBoard.map((cell:CellObject,id:number) => (
+            <Cell key={cell.pos} color={colorCell(id)} cell={cell} callback={cellOnClick}></Cell>
+        ))}
+        </div>
+        <Promotion isVisible={true} isPromotionActive={promoting} callback={promotionClick}/>
+    </>;
 }
+
+const choises = ["R","K","B","Q"];
+
+function Promotion({isVisible, isPromotionActive,callback}:{isVisible:boolean,isPromotionActive:boolean,callback:(cell:CellObject) => void}){
+    return <div className="Promotion" hidden={!isVisible}>
+        {choises.map((choise:string,id:number) => (
+            <Cell key={id} color={isPromotionActive?colorCell(1):colorCell(0)} cell={new CellObject(""+id,choise)} callback={callback}></Cell>
+        ))}
+    </div>;
+}
+
 
 export default ChessBoard;
