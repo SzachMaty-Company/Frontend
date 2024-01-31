a0d52d4331db06cb45bd778f7eea5999b3cf83f5
SzymonRogozinski <95580635+SzymonRogozinski@users.noreply.github.com>
2024-01-31 11:56:30 +0100

    feat: Authorization component

---
+++ src/AuthComponent.tsx
@@ -0,0 +1,21 @@
+const AuthComponent = {
+    isAuthenticated: false,
+    authenticate: (user:string, password:string, callback:any) => {
+      if (user === "Admin" && password === "Admin") {
+        AuthComponent.isAuthenticated = true;
+        setTimeout(() => {
+          callback("Success");
+        }, 100);
+      } else {
+        setTimeout(() => {
+          callback("Invalid data login or password");
+        }, 100);
+      }
+    },
+    unAuthenticate: (callback:any) => {
+      AuthComponent.isAuthenticated = false;
+      setTimeout(callback, 100);
+    }
+  };
+  
+  export default AuthComponent;
