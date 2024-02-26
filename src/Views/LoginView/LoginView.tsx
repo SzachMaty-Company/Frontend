559e4ac647eba22857b7e46749caca1878b42444
SzymonRogozinski <pokeszymon.pl@gmail.com>
2024-02-26 13:03:12 +0100

    Change look of login page

--- src/Views/LoginView/LoginView.tsx
+++ src/Views/LoginView/LoginView.tsx
@@ -1,59 +1,35 @@
-import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import AuthComponent from "../../AuthComponent";
+import "./LoginView.css"
 
 export default function LoginView() {
-    const mes1 = "Username ";
-    const mes2 = "Password ";
     let navigate = useNavigate();
     let location = useLocation();
-  
-    let [messageLogin, setLoginMessage] = useState("");
 
-    const registerCallback=()=>{
-        navigate("/register")
-    }
-  
     const handleSubmit = (event:any) => {
       event.preventDefault();
-      let data:FormData = new FormData(event.currentTarget);
-  
-      AuthComponent.authenticate(data.get("user") as string, data.get("password") as string, (status:any) => {
+      AuthComponent.authenticate("Admin", "Admin", (status:any) => {
         console.log(status);
         if (status === "Success") {
           if (location?.state?.from) navigate(location.state.from);
           else navigate("/");
-        } else {
-          setLoginMessage(status);
         }
       });
     };
     return (
       <div>
-        Write user data
-        <form onSubmit={handleSubmit}>
-          {mes1}
-          <input
-            onChange={() => {
-              setLoginMessage("");
-            }}
-            type="text"
-            name="user"
-          />
-          <br />
-          {mes2}
-          <input
-            onChange={() => {
-              setLoginMessage("");
-            }}
-            type="password"
-            name="password"
-          />
-          <br />
-          <div style={{ color: "red" }}>{messageLogin}</div>
-          <button type="submit">Log in</button>
-        </form>
-        <button onClick={registerCallback}>Register</button>
+        <table className="LoginTable">
+          <tr>
+            <td>
+              <h1>Logowanie</h1>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="GoogleIcon" alt="Google auth" onClick={handleSubmit}/>
+            </td>
+          </tr>
+        </table>
       </div>
     );
   }
