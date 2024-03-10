2fbe17e3f8f5d265628fa7f3b115b2de8b63bfd2
Kacper Wiączek <kacper.wiaczek00@gmail.com>
2024-03-10 14:14:36 +0100

    sddadsad

--- src/ApiHelpers/ChatServiceClient.ts
+++ src/ApiHelpers/ChatServiceClient.ts
@@ -7,16 +7,58 @@ interface ChatMessageInterface {
     date: Date;
 }
 
+interface ChatParticipant{
+    id: string,
+    username: string
+}
+
+interface ChatRoom{
+    participant: ChatParticipant,
+    messages: ChatMessageInterface[],
+    chatId: number
+}
+
+interface JwtClaims extends JwtPayload {
+  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'?: string;
+  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'?: string;
+}
+
+function tokenToId(token: string)
+{
+    const decoded = jwtDecode(token) as JwtClaims;
+    return decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+}
+
 class ChatSerivceClient{
     private stompClient: Client | undefined;
     private subscription: StompSubscription | undefined;
 
-    constructor(private token: string, private url: string){
+    constructor(private token: string, private url: string, private handleReceivedMessage: (message: ChatMessageInterface, numberArg: number) => void){
+    }
+
+    public makeMessage(text: string, idOfSender: string, date: string)
+    {
+        console.log("DEEEBUUUGD");
+        console.log(tokenToId(this.token));
+        console.log(idOfSender);
+        const parsedDate = Date.parse(date);
+        const msg : ChatMessageInterface = {
+            text: text,
+            sideOfChat: idOfSender !== tokenToId(this.token),
+            date: new Date(parsedDate)
+        };
+        return msg;
     }
 
     public showMessage(message: IMessage){
-        console.log("received message!");
-        console.log(JSON.parse(message.body));
+        const parsedMessage = JSON.parse(message.body);
+        console.log(parsedMessage);
+        const ownerId = tokenToId(this.token);
+        if (ownerId != undefined)
+        {
+            const newMessage = this.makeMessage(parsedMessage.message, parsedMessage.senderId, parsedMessage.timestamp);
+            this.handleReceivedMessage(newMessage, parsedMessage.chatId);
+        }
     }
 
     public connect(){
@@ -33,7 +75,7 @@ class ChatSerivceClient{
         this.stompClient.onConnect = (frame) => {
             if (this.stompClient)
             {
-                this.subscription = this.stompClient.subscribe("/user/queue/messages", this.showMessage);
+                this.subscription = this.stompClient.subscribe("/user/queue/messages", this.showMessage.bind(this));
             }
         }
 
@@ -64,95 +106,80 @@ class ChatSerivceClient{
             this.stompClient.deactivate();
         }
     }
-}
 
-async function makeRequest(token: string, url: string, endpoint: string){
-    let response = await fetch(`http://${url}/${endpoint}`, {
-        method: 'GET',
-        headers: {
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'}
-    });
-    return response.json();
-}
-
-interface ChatParticipant{
-    id: string,
-    username: string
-}
-
-interface ChatRoom{
-    participant: ChatParticipant,
-    messages: ChatMessageInterface[],
-    chatId: number
-}
-interface JwtClaims extends JwtPayload {
-  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'?: string;
-  'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'?: string;
-}
-function tokenToId(token: string)
-{
-    const decoded = jwtDecode(token) as JwtClaims;
-    return decoded["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
-}
-
-async function gatherParticipants(token:string, url:string, chatId: number, tokenOwnerId: string){
-    if (tokenOwnerId != undefined)
-    {
-        const participantsData = await makeRequest(token, url, `chat/${chatId}/participants`);
-        for (const participantData of participantsData)
-        {
-            if (tokenOwnerId != participantData.participantId)
-            {
-                let participant : ChatParticipant = {
-                    id: participantData.participantId,
-                    username: participantData.username
-                }
-                return participant;
-            }
-        }
+    private async makeRequest(endpoint: string){
+        let response = await fetch(`http://${this.url}/${endpoint}`, {
+            method: 'GET',
+            headers: {
+            'Authorization': `Bearer ${this.token}`,
+            'Content-Type': 'application/json'}
+        });
+        return response.json();
     }
-}
 
-async function gatherMessages(token: string, url: string){
-
-    const receivedMessages = await makeRequest(token, url, 'user/chats?page=0&size=20&sort=desc');
-
-    let chatRooms = new Map<number, ChatRoom>();
-
-    const IdOfOwnerOfTheToken = tokenToId(token);
-    for (const x of receivedMessages.content)
-    {
-        const chatid = parseInt(x.chatId);
-        if (chatRooms.has(chatid) == false)
+    private async gatherParticipants(chatId: number){
+        const tokenOwnerId = tokenToId(this.token);
+        if (tokenOwnerId != undefined)
         {
-            if (IdOfOwnerOfTheToken != undefined)
+            const participantsData = await this.makeRequest(`chat/${chatId}/participants`);
+            for (const participantData of participantsData)
             {
-                const party = await gatherParticipants(token, url, chatid, IdOfOwnerOfTheToken);
-                if (party != undefined)
+                if (tokenOwnerId != participantData.participantId)
                 {
-                    let newChatRoom : ChatRoom =  {
-                        participant: party,
-                        messages: [],
-                        chatId: chatid
+                    let participant : ChatParticipant = {
+                        id: participantData.participantId,
+                        username: participantData.username
                     }
-                    chatRooms.set(chatid, newChatRoom);
+                    return participant;
                 }
             }
         }
-        const parsedDate = Date.parse(x.messageTimestamp);
-        let message : ChatMessageInterface = {
-            text: x.message,
-            sideOfChat: x.senderId !== IdOfOwnerOfTheToken,
-            date: new Date(parsedDate)
-        };
-        let chatRoom = chatRooms.get(chatid);
-        if (chatRoom != undefined)
-        {
-            chatRoom.messages.push(message);
-        }
     }
-    return chatRooms;
+
+    public async gatherMessages(chatId: number){
+        const receivedMessages = await this.makeRequest(`chat/${chatId}/messages?sort=timestamp,desc`);
+        
+        let messages: ChatMessageInterface[] = [];
+        for (const x of receivedMessages.content)
+        {
+            messages.push(this.makeMessage(x.message, x.senderId, x.timestamp));
+        }
+        messages.sort((a, b) => a.date.getTime() - b.date.getTime());
+        return messages;
+    }
+
+    public async gatherChatRooms(token: string, url: string){
+
+        const receivedMessages = await this.makeRequest('user/chats');
+
+        let chatRooms = new Map<number, ChatRoom>();
+
+        const idOfOwnerOfTheToken = tokenToId(token);
+        for (const x of receivedMessages.content)
+        {
+            const chatid = parseInt(x.chatId);        
+            if (chatRooms.has(chatid) == false)
+            {
+                if (idOfOwnerOfTheToken != undefined)
+                {
+                    const party = await this.gatherParticipants(chatid);
+                    const messages = await this.gatherMessages(chatid);
+                    if (party != undefined)
+                    {
+                        let newChatRoom : ChatRoom =  {
+                            participant: party,
+                            messages: messages,
+                            chatId: chatid
+                        }
+                        chatRooms.set(chatid, newChatRoom);
+                    }
+                }
+            }
+        }
+        return chatRooms;
+    }
 }
 
-export {gatherMessages, ChatSerivceClient};
+
+
+export {ChatSerivceClient};
