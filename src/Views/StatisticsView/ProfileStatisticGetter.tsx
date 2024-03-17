ad80cf2026c7ba32309106ac141738bc24593fe7
SzymonRogozinski <pokeszymon.pl@gmail.com>
2024-03-17 12:50:40 +0100

    smol fix

--- src/Views/StatisticsView/ProfileStatisticGetter.tsx
+++ src/Views/StatisticsView/ProfileStatisticGetter.tsx
@@ -2,15 +2,27 @@ import AuthComponent from "../../AuthComponent";
 import ProfileStatistic from "./ProfileStatistic";
 
 export default async function GetProfileStatistic(userId:number|undefined):Promise<ProfileStatistic>{
+
+    console.log("User ID:");
+    console.log(userId);
     
-    let response = await fetch(`http://localhost:5000/statistics/user?id=${userId}`, {
+    let url=userId===undefined?`http://localhost:8000/user`:`http://localhost:8000/user?id=${userId}`;
+
+    console.log("url:");
+    console.log(url);
+
+    let response = await fetch(url, {
             method: 'GET',
             headers: {
             'Authorization': `Bearer ${AuthComponent.JSONToken}`,
             'Content-Type': 'application/json'}
         });
 
+        
     let json:string=await response.json();
+
+    console.log("JSON token:");
+    console.log(json);
 
     let profile:ProfileStatistic=JSON.parse(json);
 
