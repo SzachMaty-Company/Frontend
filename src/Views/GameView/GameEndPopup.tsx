53a0a894d2cafdf82b9de5759162943157a9d446
SzymonRogozinski <pokeszymon.pl@gmail.com>
2024-04-14 12:20:46 +0200

    Popup fix

--- src/Views/GameView/GameEndPopup.tsx
+++ src/Views/GameView/GameEndPopup.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Popup from "reactjs-popup";
 import './GameView.css'
+import { useNavigate } from "react-router-dom";
 
 export default function GamePopup({popupInfo,open}:{popupInfo:string[],open:boolean}) {
+  let navigate = useNavigate();
+  const callback=()=>{
+    navigate("/");
+  }
+
   return (
     <div>
       <Popup open={open} modal nested>
@@ -12,7 +18,7 @@ export default function GamePopup({popupInfo,open}:{popupInfo:string[],open:bool
             <p>{popupInfo[1]}</p>:
             <p></p>
           }
-          <button>Przejdź do podsumowania</button>
+          <button onClick={callback}>Przejdź do strony głównej</button>
         </div>
       </Popup>
     </div>
