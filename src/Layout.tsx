31abd40a718f9ce8f986b497bb26242467fc386a
SzymonRogozinski <95580635+SzymonRogozinski@users.noreply.github.com>
2024-04-16 19:32:58 +0200

    fix: Add friend invites
    * Działa
    
    * fix imports
    
    ---------
    
    Co-authored-by: Dawid Cybulski <100097374+davidcybulsky@users.noreply.github.com>

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -23,10 +23,49 @@ export default function Layout({gamePath,statPath,loginPath,searchPath}:{gamePat
         AuthComponent.unAuthenticate();
         navigate("/");
       //Log in
-      }else{
-        navigate(loginPath);
+    } else {
+      navigate(loginPath);
+    }
+  };
+
+  return <div>
+    <Header gameCallback={gameCallback} statCallback={statCallback} loginCallback={loginCallback} searchCallback={searchCallback} />
+    <Outlet />
+  </div>
+}
+
+function Header({ gameCallback, statCallback, loginCallback, searchCallback }: { gameCallback: any, statCallback: any, loginCallback: any, searchCallback: any }) {
+  let [cnt, setCnt] = useState(0);
+  let [gotFriendRequest, setGotFriendRequest] = useState(false);
+  let [friendId,setFriendId]=useState(-1);
+  let [showPopup,setShowPopup] = useState(false);
+  let [popupInfo,setPopupInfo] = useState([]as string[]);
+  //Pętla sprawdzania
+  useEffect(() => {
+    const intervalFiendRequest = setTimeout(async () => {
+      if (AuthComponent.isAuthenticated) {
+        let friends=await FriendRequests();
+        console.log(friends)
+        if(friends.length == 0){
+          setGotFriendRequest(false);
+        }else if(friends[0]!==friendId){
+          let friend=friends[0];
+          setPopupInfo([friend.name,friend.surname]);
+          setFriendId(friend.id);
+          setGotFriendRequest(true);
+        }
+
       }
     };
+  }, [cnt])
+
+  const AddFriendCallback=async ()=>{
+    await AcceptFriendRequest(friendId);
+    //Reload
+    setCnt(0);
+    setShowPopup(false);
+  };
+
 
     return <div>
       <Header gameCallback={gameCallback} statCallback={statCallback} loginCallback={loginCallback} searchCallback={searchCallback}/>
